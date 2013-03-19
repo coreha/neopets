0e5d6ded480316f5c2fd496cd5d1b0d44431a425
Corey <coreha@regex.be>
2013-03-18 17:23:38 -0700

    adding lookups extension/user script

---
+++ lookups/lookups.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name           Neopets - Turn Off User Lookup Code
+// @description    Removes user code from user lookups.
+// @include        http://www.neopets.com/userlookup*
+// @match          http://www.neopets.com/userlookup*
+// @version        1.00
+// @updated        31.01.2012
+// ==/UserScript==
+
+e = document.createElement("script");
+e.textContent = "jQuery('a[href*=\"/s/index.phtml?track_cat_id\"]').closest('div').nextAll('div')[0].innerHTML = '';";
+document.body.appendChild(e);
---
+++ lookups/lookups/lookups.js
@@ -0,0 +1,1 @@
+jQuery('a[href*="/s/index.phtml?track_cat_id"]').closest('div').nextAll('div')[0].innerHTML = '';
