5e8b62c54c0bc2607d896d34985cdc975117609f
Corey <coreha@regex.be>
2013-03-30 02:20:32 -0700

    Updating SSW-Popout: code improvements, minor changes

--- ssw-popout/ssw-popout.user.js
+++ ssw-popout/ssw-popout.user.js
@@ -313,9 +313,15 @@ if ( window.location.pathname == "/ssw-popout" && document.title != "SSW" ) {
 	
 	document.close();
 } else {
-	var e = document.createElement('script');
-	e.type = "text/javascript";
 	/* If not running on our page, add a popout link to the SSW */
-	e.textContent = "jQuery('#search-form #area').append('<p><a style=\"cursor: pointer;\" onclick=\"javascript: window.open(\\\'/ssw-popout\\\', \\\'SSWwindow\\\', \\\'height=284,width=612,status=0,toolbar=0,resizable=0,scrollbars=0,location=0,menubar=0,toolbar=0\\\');\">SSW Popout</a></p>')";
-	document.body.appendChild(e);
+	
+	var p = document.createElement('p');
+	var a = document.createElement('a');
+	a.setAttribute('onclick', 'javascript: window.open("/ssw-popout", "SSWwindow", "height=284,width=612,status=0,toolbar=0,resizable=0,scrollbars=0,location=0,menubar=0,toolbar=0");');
+	a.setAttribute('style', 'cursor: pointer;');
+	p.setAttribute('style', 'width: 100%; text-align: center; clear: both;');
+	a.textContent = "SSW Popout";
+
+	p.appendChild(a);
+	document.getElementById('area').appendChild(p);
 }
